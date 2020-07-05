a78d2fb50604726cc469c4adb51dab96d3352d0a
Vivek Kandasamy <vivekkandasamy@gmx.de>
2020-07-05 21:32:05 +0200

    Add files via upload

---
+++ Data structures and Algorithms nanodegree/2. Data Structures/Problem1_LRU_Cache.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  5 16:58:43 2020
+
+@author: vivek
+"""
+import collections
+
+class LRU_Cache(object):
+
+    def __init__(self, capacity):
+        # Initialize class variables
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent. 
+        if key in self.cache:
+            value=self.cache.pop(key)
+            self.cache[key]=value
+            return self.cache[key]
+        return -1
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        if key in self.cache:
+            self.cache.pop(key)
+            self.cache[key]=value
+            return
+        
+        if len(self.cache.keys())==self.capacity:
+            self.cache.popitem(last=False)
+            
+        self.cache[key]=value
+        return
+        
+        
+our_cache = LRU_Cache(5)
+
+our_cache.set(1, 1);
+our_cache.set(2, 2);
+our_cache.set(3, 3);
+our_cache.set(4, 4);
+
+
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+
+our_cache.set(5, 5)
+
+our_cache.set(6, 6)
+
+print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+
+our_cache.set(4, 7)
+
+print(our_cache.get(4))
---
+++ Data structures and Algorithms nanodegree/2. Data Structures/Problem4_ Active_Directory.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  5 19:40:55 2020
+
+@author: vivek
+"""
+
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    if user in group.get_users():
+        return True
+    
+    for group in group.get_groups():
+        return is_user_in_group(user, group)
+    
+    return False
+
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child.add_group(sub_child)
+parent.add_group(child)
+
+print(is_user_in_group('vvv',parent)) #returns False
+
+print(is_user_in_group('sub_child_user',parent)) #returns True
+
+parent.add_user('vvv')
+print(is_user_in_group('vvv',parent)) # returns True
