e7c7c575446ac90e4bbc52f5913097abfaee8870
Vivek Kandasamy <vivekkandasamy@gmx.de>
2020-07-09 00:48:13 +0200

    Update Problem1_LRU_Cache.py

--- Data structures and Algorithms nanodegree/2. Data Structures/Problem1_LRU_Cache.py
+++ Data structures and Algorithms nanodegree/2. Data Structures/Problem1_LRU_Cache.py
@@ -23,6 +23,8 @@ class LRU_Cache(object):
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        if self.capacity<=0:
+            return
         if key in self.cache:
             self.cache.pop(key)
             self.cache[key]=value
@@ -36,23 +38,29 @@ class LRU_Cache(object):
         
         
 our_cache = LRU_Cache(5)
-
 our_cache.set(1, 1);
 our_cache.set(2, 2);
 our_cache.set(3, 3);
 our_cache.set(4, 4);
-
-
 print(our_cache.get(1))       # returns 1
 print(our_cache.get(2))       # returns 2
 print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
-
 our_cache.set(5, 5)
-
 our_cache.set(6, 6)
-
 print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
 
-our_cache.set(4, 7)
+our_cache=LRU_Cache(0)
+our_cache.set(1, 1);
+print(our_cache.get(1))     # returns -1 because the cache cannot store any element
 
-print(our_cache.get(4))
+our_cache = LRU_Cache(7)
+our_cache.set(1, 1);
+our_cache.set(2, 2);
+our_cache.set(3, 3);
+our_cache.set(4, 4);
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+our_cache.set(5, 5)
+our_cache.set(6, 6)
+print(our_cache.get(3))      # returns 3
